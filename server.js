1ba18a7b8667936d195425c48699f67b957ae81d
Manoj kumar <61312222+rmkdn@users.noreply.github.com>
2020-05-03 13:07:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const http = require('http');
-
 const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end('Hello, World!');
+res.writeHead(200);
+res.end('Hello, World!');
 }
 
 const server = http.createServer(requestListener);
